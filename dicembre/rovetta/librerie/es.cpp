ce3aa3fef70cd9e32679b3cf5961fc7fc68eeef5
simone3c <80486631+simone3c@users.noreply.github.com>
2021-03-11 21:45:15 +0100

    Delete dicembre directory

--- dicembre/rovetta/librerie/es.cpp
+++
@@ -1,7 +0,0 @@
-#include "print_hello.h"//cearcto nella directory corrente
-
-int main(){
-
-	printhello();
-	
-}
--- dicembre/rovetta/librerie/print_hello.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-void printhello(){
-
-	std::cout << "hello world\n";
-}
-	
